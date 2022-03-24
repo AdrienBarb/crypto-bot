dcd56f446242e3a85d813e5fbd872b230ba744eb
Adrien Barbier <a.barbier@archeon-medical.com>
2022-03-24 16:51:45 +0100

    fix

---
+++ swapTokensForTokens.js
@@ -0,0 +1,137 @@
+const inquirer = require("inquirer");
+const ethers = require("ethers");
+const { mnemonicToEntropy } = require("ethers/lib/utils");
+require("dotenv").config();
+const prompt = require("prompt-sync")({ sigint: true });
+
+//ADRESSES
+const addresses = {
+  router: "0x10ed43c718714eb63d5aa57b78b54704e256024e",
+  BNB: "0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c",
+  STABLE: "0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c",
+  TOKEN_TO_SNIPE: "0xdb42ff764E0B1e1D63F10135FB0Cf1F848d428DE",
+  PAIR: "0xa0feB3c81A36E885B6608DF7f0ff69dB97491b58",
+  FACTORY: "0xcA143Ce32Fe78f1f7019d7d551a6402fC5350c73",
+  WALLET_ADDRESS: process.env.WALLET_ADRESS,
+};
+
+//GAS
+const gasPrice = ethers.utils.parseUnits("5", "gwei");
+const gas = {
+  gasPrice: gasPrice,
+  gasLimit: 600000,
+};
+
+//ACCOUNT
+const mnemonic = process.env.SEED_PHRASE;
+const provider = new ethers.providers.WebSocketProvider(
+  process.env.SERVER_NODE
+);
+const wallet = ethers.Wallet.fromMnemonic(mnemonic);
+const account = wallet.connect(provider);
+
+//CONTRACTS
+const routerContract = new ethers.Contract(
+  addresses.router,
+  [
+    "function getAmountsOut(uint amountIn, address[] memory path) public view returns(uint[] memory amounts)",
+    "function swapExactTokensForTokens(uint amountIn, uint amountOutMin, address[] calldata path, address to, uint deadline) external returns (uint[] memory amounts)",
+    "function swapExactETHForTokensSupportingFeeOnTransferTokens(uint amountIn, uint amountOutMin, address[] calldata path, address to, uint deadline) external returns (uint[] memory amounts)",
+    "function swapExactETHForTokens(uint amountOutMin, address[] calldata path, address to, uint deadline) external payable returns (uint[] memory amounts)",
+    "function swapExactTokensForETH(uint amountIn, uint amountOutMin, address[] calldata path, address to, uint deadline)",
+  ],
+  account
+);
+
+const factoryContract = new ethers.Contract(
+  addresses.FACTORY,
+  [
+    "function getPair(address tokenA, address tokenB) external view returns (address pair)",
+  ],
+  account
+);
+
+const getBalanceSellingOfToken = async (tokenContract) => {
+  const ABI = [
+    {
+      constant: true,
+      inputs: [{ name: "_owner", type: "address" }],
+      name: "balanceOf",
+      outputs: [{ name: "balance", type: "uint256" }],
+      type: "function",
+    },
+  ];
+  const newContract = new ethers.Contract(tokenContract, ABI, provider);
+  const balance = await newContract.balanceOf(addresses.WALLET_ADDRESS);
+  const balanceFormatted = ethers.utils.formatEther(balance);
+  console.log(balanceFormatted);
+  return balanceFormatted;
+};
+
+const sellToken = async () => {
+  const sellingTokenContract = new ethers.Contract(
+    addresses.TOKEN_TO_SNIPE,
+    [
+      "function approve(address spender, uint256 amount) external returns (bool)",
+    ],
+    account
+  );
+
+  const balanceOfTokenToSell = await getBalanceSellingOfToken(
+    addresses.TOKEN_TO_SNIPE
+  );
+
+  console.log("Balance of token to sell ", balanceOfTokenToSell);
+
+  const SELLTOKENAmountIn = ethers.utils.parseUnits(
+    `${balanceOfTokenToSell}`,
+    18
+  );
+  let amounts = await routerContract.getAmountsOut(SELLTOKENAmountIn, [
+    addresses.TOKEN_TO_SNIPE,
+    addresses.STABLE,
+  ]);
+  const BUYTOKENamountOutMin = amounts[1].sub(amounts[1].div(4));
+
+  console.log(ethers.utils.formatEther(SELLTOKENAmountIn));
+  console.log(ethers.utils.formatEther(BUYTOKENamountOutMin));
+
+  const approveTx = await sellingTokenContract.approve(
+    addresses.router,
+    SELLTOKENAmountIn
+  );
+  await approveTx.wait();
+
+  console.log(`Swapping tokens...`);
+  const swapTx = await routerContract.swapExactTokensForTokens(
+    SELLTOKENAmountIn,
+    BUYTOKENamountOutMin,
+    [addresses.TOKEN_TO_SNIPE, addresses.STABLE],
+    addresses.WALLET_ADDRESS,
+    Date.now() + 1000 * 60 * 10,
+    { ...gas }
+  );
+
+  const receipt = await swapTx.wait();
+  console.log(`Transaction hash: ${receipt.transactionHash}`);
+};
+
+inquirer
+  .prompt([
+    {
+      type: "rawlist",
+      name: "cryptoCommand",
+      message: "What do you want?",
+      choices: ["Sell token"],
+    },
+  ])
+  .then((answers) => {
+    console.info("Answer:", answers.cryptoCommand);
+    switch (answers.cryptoCommand) {
+      case "Sell token":
+        sellToken();
+        break;
+      default:
+        break;
+    }
+  });
